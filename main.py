cb0f7fee5b1916bb856588aa92339308fc82af1b
AlgoClassWork <kyxec.play@gmail.com>
2025-05-27 13:28:40 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ def home():
     return render_template('home_page.html')
 
 #http://127.0.0.1:5000/start_quiz
-
+@app.route('/start_quiz')
+def start():
+    questions = database.get_questions()
+    return render_template('start_quiz.html', questions=questions)
 
 database.create_table()
 app.run()
